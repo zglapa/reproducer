27d1631485a856a5ba36196655e389cf0502a36d
Zofia Glapa <zofia.glapa@swmansion.com>
2023-10-09 16:59:39 +0200

    Create crash repro

--- ReproducerApp/App.tsx
+++ ReproducerApp/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -6,113 +7,33 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import {SafeAreaView, View} from 'react-native';
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    <SafeAreaView>
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          paddingLeft: 20,
+          flexDirection: 'row',
+          backgroundColor: 'red',
+        }}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <View
+        style={{
+          width: 200,
+          height: 100,
+          marginTop: -20,
+          marginLeft: -20,
+          backgroundColor: 'yellow',
+          position: 'absolute',
+          zIndex: -1,
+        }}
+      />
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
--- ReproducerApp/index.js
+++ ReproducerApp/index.js
@@ -2,8 +2,31 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, View, Text} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import React from 'react';
 
+const InitialParamsContext = React.createContext(undefined);
+
+AppRegistry.setWrapperComponentProvider(appParams => {
+  return ({children, ...otherProps}) => (
+    <InitialParamsContext.Provider value={otherProps.initialProps}>
+      <View style={{width: '100%', height: '100%'}} {...otherProps}>
+        <View style={{width: '100%', height: 24}}>
+          <Text
+            style={{
+              width: '100%',
+              height: '100%',
+              fontSize: 10,
+              color: 'white',
+            }}>
+            TEXT
+          </Text>
+        </View>
+        {children}
+      </View>
+    </InitialParamsContext.Provider>
+  );
+});
 AppRegistry.registerComponent(appName, () => App);
